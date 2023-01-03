d973547306a7f95054a4aaf5ccca4fe6c1e5ac3b
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2023-01-03 11:45:51 +0100

    fix code

--- Iteración1.js
+++ Iteración1.js
@@ -3,25 +3,27 @@
 // const button$$ = document.createElement("button");
 //     console.log(button$$);
 // button$$.setAttribute("id", "btnToClick");
-const buttonClick = document.body.querySelector("#btnToClick");
-addEventListener('click' , evenToClick);
+const buttonClick = document.querySelector(".btnToClick");
 function evenToClick(event) {
     console.log(event);
     console.log("heloclick")
 }
+buttonClick.addEventListener('click' , evenToClick);
+
 
 
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
-const selecFocus = document.body.querySelector(".focus");
-selecFocus.addEventListener('focus', inputFocus);
+const selecFocus = document.querySelector(".focus");
 function inputFocus(event) {
 
     console.log('hacemos focus y su valor', event.target.value);
 }
+selecFocus.addEventListener('focus', inputFocus);
+
 
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
-const selecInput = document.body.querySelector(".input");
+const selecInput = document.querySelector(".value");
 selecInput.addEventListener('input' , function (event) {
-
+    
     console.log('Valor del input', event.target.value);
 });
